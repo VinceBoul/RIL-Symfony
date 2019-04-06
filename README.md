3757dab0bd96f82e089ec158e7eac608366361c4
Vincent <toullec.0104@apside-groupe.com>
2019-04-06 16:00:34 +0200

    Update Readme + Entité Recette + CRUD Recette

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # RIL-Symfony
+
+## Projet de recettes 
+
+### Partie I - Création des entités
+
+**- Créer l'entité Recette**
+
+Propriétés de l'entité Recette : 
+
+  * Nom
+  * Accroche
+  * Difficulté
+  * Texte
+  * Durée totale
+  * Durée préparation
+  * Durée cuisson
+  * Images
+  * Ingrédients
+
+  
+**- Créer les pages du CRUD**
+
+**- Créer plusieurs recette de test**
+
+### Partie II - Templating
+
+**- Installer un framework HTML**
+
+**- Styliser la page d'accueil**
+
+**- Améliorer la liste des résultats de recette**
+* Ajouter le nombre total de recettes dans la liste
+
+
+### Partie III
+
+**- Créer l'entité *Ingredient***
+Propriétés de l'entité Ingredient: 
+
+  * Nom
+  * Image
+
+**- Lier l'entité *Ingredient* à l'entité *Recette***
+
+### Partie IV
+
+**- Configurer la connexion utilisateur**
+
+**- Ajouter la propriété *Auteur* à l'entité Recette**
+
+**- Ajouter un système de commentaires**
---
+++ src/Controller/RecetteController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recette;
+use App\Form\RecetteType;
+use App\Repository\RecetteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/recette")
+ */
+class RecetteController extends AbstractController
+{
+    /**
+     * @Route("/", name="recette_index", methods={"GET"})
+     */
+    public function index(RecetteRepository $recetteRepository): Response
+    {
+        return $this->render('recette/index.html.twig', [
+            'recettes' => $recetteRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="recette_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $recette = new Recette();
+        $form = $this->createForm(RecetteType::class, $recette);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($recette);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recette_index');
+        }
+
+        return $this->render('recette/new.html.twig', [
+            'recette' => $recette,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="recette_show", methods={"GET"})
+     */
+    public function show(Recette $recette): Response
+    {
+        return $this->render('recette/show.html.twig', [
+            'recette' => $recette,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="recette_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Recette $recette): Response
+    {
+        $form = $this->createForm(RecetteType::class, $recette);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('recette_index', [
+                'id' => $recette->getId(),
+            ]);
+        }
+
+        return $this->render('recette/edit.html.twig', [
+            'recette' => $recette,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="recette_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Recette $recette): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recette->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($recette);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('recette_index');
+    }
+}

e8cb7f9d90a520ffcd481766d63cf6aa6a34e19b
Vincent <toullec.0104@apside-groupe.com>
2019-04-14 16:49:52 +0200

    Tempalte recette + UploadedFile annotation

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -8,6 +8,7 @@ use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -40,7 +41,7 @@ class RecetteController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
 			// $file stores the uploaded PDF file
-			/** @var Symfony\Component\HttpFoundation\ $file */
+			/** @var UploadedFile $file */
 			$file = $recette->getImage();
 
 			$fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();

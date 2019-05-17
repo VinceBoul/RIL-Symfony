9fcfab5a17d9bc562f05abe96eb69e57e517d805
Vincent <toullec.0104@apside-groupe.com>
2019-05-17 14:48:55 +0200

    Test de connexion et accès à l'édition d'une page

--- tests/RecetteControllerTest.php
+++ tests/RecetteControllerTest.php
@@ -9,23 +9,56 @@
 namespace App\Tests;
 
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class RecetteControllerTest extends WebTestCase
 {
+	/**
+	 * @var Client
+	 */
+	private $client = null;
+
+	public function setUp()
+	{
+		$this->client = static::createClient();
+	}
 
 	public function testHomePage(){
 
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', "/");
+		$crawler = $this->client->request('GET', "/");
 
 		$this->assertSame(200,
-			$client->getResponse()->getStatusCode(), "Page d'accueil OK"	);
+			$this->client->getResponse()->getStatusCode(), "Page d'accueil OK"	);
 
 		$this->assertContains("Bienvenue",
 			$crawler->filter('h1')->text(),
 			"Message d'accueil OK");
+	}
+
+	public function testNewRecette(){
+		$this->logIn();
+
+		$crawler = $this->client->request('GET', '/fr/recette/new');
+
+		$this->assertTrue($this->client->getResponse()->isSuccessful());
+	}
+
+	private function logIn()
+	{
+		$session = $this->client->getContainer()->get('session');
+
+		// Nom du firewall ( voir config/packages/security.yaml
+		$firewall = 'main';
+
+		$token = new UsernamePasswordToken('vince', null, $firewall, array('ROLE_USER'));
+		$session->set('_security_'.$firewall, serialize($token));
+		$session->save();
+
+		$cookie = new Cookie($session->getName(), $session->getId());
+		$this->client->getCookieJar()->set($cookie);
 	}
 
 	public function testCreateRecette(){
@@ -41,15 +74,14 @@ class RecetteControllerTest extends WebTestCase
 		$this->assertSame(200,
 			$client->getResponse()->getStatusCode(),
 			"Test d'affichage de la page"	);
-/*
-		$form = $crawler->selectButton('submit')->form();
+		/*
+				$form = $crawler->selectButton('submit')->form();
 
-		// set some values
-		$form['name'] = 'Crêpes bretonnes';
+				// set some values
+				$form['name'] = 'Crêpes bretonnes';
 
-// submit the form
-		$crawler = $client->submit($form);*/
+		// submit the form
+				$crawler = $client->submit($form);*/
 	}
-
 
 }
